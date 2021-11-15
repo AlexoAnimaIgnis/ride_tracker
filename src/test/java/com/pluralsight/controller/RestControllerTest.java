dbce266a6e97cefade61a67fb847c1b7066fd64f
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2021-11-15 18:00:51 +0800

    Updated the get method to use jdbc template

--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -13,7 +13,7 @@ import org.junit.Test;
 
 public class RestControllerTest {
 
-	@Test(timeout=3000)
+	@Test(timeout=30000)
 	public void testGetRides() {
 		RestTemplate restTemplate = new RestTemplate();
 
@@ -33,9 +33,10 @@ public class RestControllerTest {
 		RestTemplate restTemplate = new RestTemplate();
 
 		Ride ride = new Ride();
-		ride.setName("Round Valley Ride");
+		ride.setName("Test Valley Ride");
 		ride.setDuration(45);
 
 		restTemplate.put("http://localhost:8080/ride_tracker_war/ride", ride);
 	}
+
 }
